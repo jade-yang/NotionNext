436fb874b4e9db039b761decbeac547bcb0d11c8
Jade-Yang <73221244+jade-yang@users.noreply.github.com>
2024-06-30 16:52:15 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'Yang',
   HEO_HERO_TITLE_4: '欢迎光临',
   HEO_HERO_TITLE_5: '开心每一天( •̀ ω •́ )y',
-  // HEO_HERO_TITLE_LINK: '',
+  HEO_HERO_TITLE_LINK: 'https://pic.yupoo.com/jadeyy/83594a38/36debdf5.jpg',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  // HERO_RECOMMEND_COVER: https://pic.yupoo.com/jadeyy/4261cada/3001b51b.jpeg,
+  HERO_RECOMMEND_COVER: https://pic.yupoo.com/jadeyy/4261cada/3001b51b.jpeg,
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -48,7 +48,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/jade-yang',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://github.com/jade-yang,
+  HEO_INFO_CARD_URL3: 'https://github.com/jade-yang',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
